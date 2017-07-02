634254320dcac1fd6665a5e7cbcd9ef29313afcf
Sahil Shah <shahsahil007@gmail.com>
2017-07-02 15:03:56 +0530

    v1

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from flask import render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template;
+from os import environ
 import json
+
+app = Flask(__name__)
+
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html', user=user)
+	return render_template('index.html')
+	
+HOST = environ.get('SERVER_HOST', 'localhost')
+#app.secret_key = "SuPeR_SeCReT_KeY"
+try:
+	PORT = int(environ.get('SERVER_PORT', '5555'))
+except ValueError:
+	PORT = 5555
+app.run(HOST, PORT, debug=True, threaded=True)
+
